ac5ecb1ef86dc7fd95f4873ee00e7263cdc95497
Ramon Klass <tier@schokokeks.org>
2012-12-13 21:38:53 +0100

    very simple setup.py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='clii',
+    version='0.1',
+    url='https://github.com/Gentle/clii',
+    author='Ramon Klass',
+    author_email='tier@schokokeks.org',
+    description='commandline helper utilities',
+    packages=['clii']
+)
