6b370b36d1a3f7e3544a729334679700ae9b4e52
Ramon Klass <tier@schokokeks.org>
2012-12-13 21:02:37 +0100

    terminal size query code taken from http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python

---
+++ clii/util.py
@@ -0,0 +1,29 @@
+import readline
+
+# http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
+def getTerminalSize():
+    """
+    taken from stackoverflow.
+    this function returns a tuple (y, x) for the screen dimensions
+    """
+    import os
+    env = os.environ
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl, termios, struct, os
+            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
+                                                 '1234'))
+        except:
+            return
+        return cr
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+    if not cr:
+        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+    return int(cr[1]), int(cr[0])
